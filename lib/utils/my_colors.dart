9f0403e9c957efa8af8dc1c9680783033a011bfc
Kayky Barbosa <kaykyb629@gmail.com>
2024-03-17 18:31:49 -0300

    refactor: pequenas alterações

--- lib/utils/my_colors.dart
+++ lib/utils/my_colors.dart
@@ -3,10 +3,12 @@ import 'package:flutter/material.dart';
 class MyColors {
   MyColors._();
 
+  static Color primary = const Color(0xFF000000);
+
   static Color scaffoldColor = Colors.grey.shade300;
 
-  static Color base100 = Colors.white;
-  static Color base300 = Colors.grey;
+  static const Color base100 = Colors.white;
+  static const Color base300 = Colors.grey;
   static Color base300Shade800 = Colors.grey.shade800;
   static Color base300Shade200 = Colors.grey.shade200;
 
