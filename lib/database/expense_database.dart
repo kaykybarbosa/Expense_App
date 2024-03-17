594e062dca5dce398b9a693f3cace8a2962ed882
Kayky Barbosa <kaykyb629@gmail.com>
2024-03-17 18:26:10 -0300

    refactor: removendo os método do db

--- lib/database/expense_database.dart
+++ lib/database/expense_database.dart
@@ -1,11 +1,11 @@
+import 'dart:developer';
+
 import 'package:expense_app/models/expense.dart';
-import 'package:flutter/material.dart';
 import 'package:isar/isar.dart';
 import 'package:path_provider/path_provider.dart';
 
-class ExpenseDatabase extends ChangeNotifier {
+class ExpenseDatabase {
   static late Isar isar;
-  final List<Expense> _allExpense = [];
 
   /*
 
@@ -21,114 +21,29 @@ class ExpenseDatabase extends ChangeNotifier {
 
   /*
 
-  G E T T E R S
-
-  */
-
-  List<Expense> get allExpense => _allExpense;
-
-  /*
-
   O P E R A T I O N S
 
   */
 
   // Create - add a new expense
   Future<void> createExpense(Expense expense) async {
-    await isar.writeTxn(() => isar.expenses.put(expense));
+    await isar.writeTxn(() async => await isar.expenses.put(expense));
 
-    await getAllExpenses();
+    log('ADICIONOU EXPENSE', name: 'GRAPH TEST');
   }
 
   // Read - expense fom db
-  Future<void> getAllExpenses() async {
-    List<Expense> expenses = await isar.expenses.where().findAll();
-
-    _allExpense.clear();
-    _allExpense.addAll(expenses);
-
-    notifyListeners();
-  }
+  Future<List<Expense>> getAllExpenses() async => await isar.expenses.where().findAll();
 
   // Update - edit an expense in db
   Future<void> updateExpense({required int id, required Expense expense}) async {
     expense.id = id;
 
-    await isar.writeTxn(() => isar.expenses.put(expense));
-
-    await getAllExpenses();
+    await isar.writeTxn(() async => await isar.expenses.put(expense));
   }
 
   // Delete -  an expense
   Future<void> deleteExpense({required int id}) async {
-    await isar.writeTxn(() => isar.expenses.delete(id));
-
-    await getAllExpenses();
-  }
-
-  /*
-
-  H E L P E R S
-
-  */
-
-  // calculate total expense for each month
-  Future<Map<String, double>> calculateMonthlyTotals() async {
-    await getAllExpenses();
-
-    Map<String, double> monthlyTotals = {};
-
-    for (var expense in _allExpense) {
-      String yearMonth = '${expense.date.year}-${expense.date.month}';
-
-      if (!monthlyTotals.containsKey(yearMonth)) {
-        monthlyTotals[yearMonth] = 0;
-      }
-
-      monthlyTotals[yearMonth] = monthlyTotals[yearMonth]! + expense.amount;
-    }
-
-    return monthlyTotals;
-  }
-
-  // get start month
-  int getStartMonth() {
-    if (_allExpense.isEmpty) {
-      return DateTime.now().month;
-    }
-
-    _allExpense.sort((a, b) => a.date.compareTo(b.date));
-
-    return _allExpense.first.date.month;
-  }
-
-  // get start year
-  int getStartYear() {
-    if (_allExpense.isEmpty) {
-      return DateTime.now().year;
-    }
-
-    _allExpense.sort((a, b) => a.date.compareTo(b.date));
-
-    return _allExpense.first.date.year;
-  }
-
-  // calculate current month total
-  Future<double> calculateCurrentMonthExpenses() async {
-    await getAllExpenses();
-
-    var now = DateTime.now();
-    int currentMonth = now.month;
-    int currentYear = now.year;
-
-    List<Expense> currentMonthExpenses = _allExpense
-        .where(
-          (expense) => expense.date.month == currentMonth && expense.date.year == currentYear,
-        )
-        .toList();
-
-    double total = currentMonthExpenses.fold(0, (sum, expense) => sum + expense.amount);
-
-    return total;
+    await isar.writeTxn(() async => await isar.expenses.delete(id));
   }
 }
