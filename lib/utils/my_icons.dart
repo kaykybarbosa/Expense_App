3b829fbb86528385f8948d72b1f0f9e4aa5787a5
Kayky Barbosa <kaykyb629@gmail.com>
2024-03-23 20:32:34 -0300

    add: ícone home

--- lib/utils/my_icons.dart
+++ lib/utils/my_icons.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class MyIcons {
   MyIcons._();
 
+  static const IconData home = Icons.home;
   static const IconData add = Icons.add;
   static const IconData settings = Icons.settings;
   static const IconData delete = Icons.delete;
