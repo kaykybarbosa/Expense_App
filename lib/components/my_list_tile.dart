cd4ffb2ca176373ba8f2363f854735862b323299
Kayky Barbosa <kaykyb629@gmail.com>
2024-03-23 20:41:27 -0300

    refactor: adaptando para o tema escuro

--- lib/components/my_list_tile.dart
+++ lib/components/my_list_tile.dart
@@ -1,5 +1,6 @@
+import 'package:expense_app/components/custom_container.dart';
+import 'package:expense_app/domain/models/expense.dart';
 import 'package:expense_app/helper/helper_functions.dart';
-import 'package:expense_app/models/expense.dart';
 import 'package:expense_app/utils/constants.dart';
 import 'package:expense_app/utils/my_colors.dart';
 import 'package:expense_app/utils/my_icons.dart';
@@ -20,7 +21,10 @@ class MyListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Padding(
-        padding: const EdgeInsets.symmetric(horizontal: Constants.defaultMargin, vertical: 5),
+        padding: const EdgeInsets.symmetric(
+          vertical: 5,
+          horizontal: Constants.defaultMargin,
+        ),
         child: Slidable(
           endActionPane: ActionPane(
             motion: const StretchMotion(),
@@ -28,35 +32,30 @@ class MyListTile extends StatelessWidget {
               SlidableAction(
                 icon: MyIcons.settings,
                 onPressed: onEditPressed,
-                backgroundColor: MyColors.base300,
-                foregroundColor: MyColors.base100,
-                borderRadius: BorderRadius.circular(5),
+                backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
+                foregroundColor: Theme.of(context).colorScheme.onPrimary,
+                borderRadius: BorderRadius.circular(Constants.borderRadius),
               ),
               SlidableAction(
                 icon: MyIcons.delete,
                 onPressed: onDeletePressed,
-                backgroundColor: MyColors.alert,
-                foregroundColor: MyColors.base100,
-                borderRadius: BorderRadius.circular(5),
+                backgroundColor: Theme.of(context).colorScheme.errorContainer,
+                foregroundColor: Theme.of(context).colorScheme.onPrimary,
+                borderRadius: BorderRadius.circular(Constants.borderRadius),
               ),
             ],
           ),
-          child: Container(
-            decoration: BoxDecoration(
-              color: MyColors.base300Shade200,
-              borderRadius: BorderRadius.circular(5),
-              boxShadow: [
-                BoxShadow(
-                  color: MyColors.base300.withOpacity(.2),
-                  blurRadius: 5,
-                )
-              ],
-            ),
+          child: CustomContainer(
+            margin: EdgeInsets.zero,
+            padding: EdgeInsets.zero,
             child: ListTile(
               title: Text(expense.name),
               subtitle: Text(
                 formatDate(expense.date),
-                style: const TextStyle(color: MyColors.base300, fontSize: 13),
+                style: const TextStyle(
+                  color: MyColors.base300,
+                  fontSize: 13,
+                ),
               ),
               trailing: Text(
                 formatAmount(expense.amount),
