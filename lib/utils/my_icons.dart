307e5ecdb6301e767bf252eb3f07faed459f11a0
Kayky Barbosa <kaykyb629@gmail.com>
2024-03-24 18:18:45 -0300

    refactor: alterando os ícones do app

--- lib/utils/my_icons.dart
+++ lib/utils/my_icons.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:icons_plus/icons_plus.dart';
 
 class MyIcons {
   MyIcons._();
 
-  static const IconData home = Icons.home;
-  static const IconData add = Icons.add;
-  static const IconData settings = Icons.settings;
-  static const IconData delete = Icons.delete;
+  // static const IconData home = Icons.home;
+  // static const IconData add = Icons.add;
+  // static const IconData settings = Icons.settings;
+  // static const IconData delete = Icons.delete;
+
+  /// H E R O - I C O N S
+  static const IconData iconApp = FontAwesome.chart_simple_solid;
+  static const IconData help = Icons.help;
+  static const IconData info = Icons.info;
+  static const IconData home = HeroIcons.home;
+  static const IconData add = HeroIcons.plus;
+  static const IconData settings = HeroIcons.cog_8_tooth;
+  static const IconData delete = HeroIcons.trash;
+  static const IconData moon = HeroIcons.moon;
+  static const IconData copyright = FontAwesome.copyright;
 }
