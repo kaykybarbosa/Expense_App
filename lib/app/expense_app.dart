1c475d9522476f9db974773566446fc3130f789a
Kayky Barbosa <kaykyb629@gmail.com>
2024-03-24 11:59:19 -0300

    feat: adicionando a verificação do tema atual do app

--- lib/app/expense_app.dart
+++ lib/app/expense_app.dart
@@ -1,16 +1,38 @@
 import 'package:expense_app/app/routes/app_pages.dart';
 import 'package:expense_app/app/theme/app_theme.dart';
+import 'package:expense_app/dependency_injection/app_component.dart';
+import 'package:expense_app/pages/settings/controllers/theme_controller.dart';
 import 'package:flutter/material.dart';
 
-class ExpenseApp extends StatelessWidget {
+class ExpenseApp extends StatefulWidget {
   const ExpenseApp({super.key});
+
+  @override
+  State<ExpenseApp> createState() => _ExpenseAppState();
+}
+
+class _ExpenseAppState extends State<ExpenseApp> {
+  late ThemeController _themeController;
+
+  @override
+  void initState() {
+    super.initState();
+    _themeController = getIt<ThemeController>();
+    _themeController.getThemeMode();
+    _themeController.addListener(() {
+      if (mounted) {
+        setState(() {});
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) => MaterialApp.router(
         title: 'Expense App',
+        themeMode: _themeController.themeMode,
         theme: AppTheme.light,
         darkTheme: AppTheme.dark,
-        debugShowCheckedModeBanner: false,
         routerConfig: AppPages.pages,
+        debugShowCheckedModeBanner: false,
       );
 }
