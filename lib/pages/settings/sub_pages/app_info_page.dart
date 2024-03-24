b2ed6d0c0edb58aef4cb639d1f2393efab3c037d
Kayky Barbosa <kaykyb629@gmail.com>
2024-03-24 18:19:08 -0300

    feat: telas de help e app info

---
+++ lib/pages/settings/sub_pages/app_info_page.dart
@@ -0,0 +1,53 @@
+import 'package:expense_app/utils/constants.dart';
+import 'package:expense_app/utils/my_colors.dart';
+import 'package:expense_app/utils/my_icons.dart';
+import 'package:flutter/material.dart';
+
+class AppInfoPage extends StatelessWidget {
+  const AppInfoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) => const Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Expenses',
+                style: TextStyle(
+                  fontSize: Constants.largeFontSize,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              Text(
+                'Version 1.0',
+                style: TextStyle(color: MyColors.base300),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 25),
+                child: Icon(
+                  MyIcons.iconApp,
+                  color: MyColors.base100,
+                  // color: Theme.of(context).colorScheme.primary,
+                  size: 100,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    MyIcons.copyright,
+                    size: 15,
+                    color: MyColors.base300,
+                  ),
+                  Text(
+                    ' 2024 Kbuloso Ldta.',
+                    style: TextStyle(color: MyColors.base300),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      );
+}
---
+++ lib/pages/settings/sub_pages/help_page.dart
@@ -0,0 +1,42 @@
+import 'package:expense_app/app/routes/app_routes.dart';
+import 'package:expense_app/components/custom_container.dart';
+import 'package:expense_app/utils/constants.dart';
+import 'package:expense_app/utils/my_icons.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class HelpPage extends StatelessWidget {
+  const HelpPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    List<Map<String, dynamic>> options = [
+      {
+        'leading': MyIcons.info,
+        'title': 'App info',
+      }
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('HELP'),
+      ),
+      body: Column(
+        children: options
+            .map((e) => CustomContainer(
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: Constants.defaultMargin,
+                  ),
+                  child: ListTile(
+                    leading: Icon(e['leading']),
+                    title: Text(e['title']),
+                    onTap: () => GoRouter.of(context).push(AppRoutes.appInfo),
+                  ),
+                ))
+            .toList(),
+      ),
+    );
+  }
+}
