ac4737826cbcf8e73fbd89f0df8ed2eeca0d748f
Kayky Barbosa <kaykyb629@gmail.com>
2024-10-14 12:06:56 -0300

    refactor: removendo focusNode desnecessário

--- lib/components/custom_text_field.dart
+++ lib/components/custom_text_field.dart
@@ -1,47 +1,27 @@
 import 'package:flutter/material.dart';
 
-class CustomTextField extends StatefulWidget {
+class CustomTextField extends StatelessWidget {
   const CustomTextField({
     super.key,
     required this.label,
     this.isRequiredFocus = false,
-    this.focusNode,
     this.controller,
     this.keyboardType,
+    this.textInputAction = TextInputAction.next,
   });
 
   final String label;
   final bool isRequiredFocus;
-  final FocusNode? focusNode;
   final TextInputType? keyboardType;
   final TextEditingController? controller;
-
-  @override
-  State<CustomTextField> createState() => _CustomTextFieldState();
-}
-
-class _CustomTextFieldState extends State<CustomTextField> {
-  late final FocusNode _focusNode;
-
-  @override
-  void initState() {
-    super.initState();
-
-    if (widget.isRequiredFocus) {
-      _focusNode = FocusNode();
-
-      Future.delayed(
-        const Duration(milliseconds: 400),
-        () => _focusNode.requestFocus(),
-      );
-    }
-  }
+  final TextInputAction? textInputAction;
 
   @override
   Widget build(BuildContext context) => TextField(
-        focusNode: widget.isRequiredFocus ? _focusNode : widget.focusNode,
-        controller: widget.controller,
-        decoration: InputDecoration(label: Text(widget.label)),
-        keyboardType: widget.keyboardType,
+        autofocus: isRequiredFocus,
+        controller: controller,
+        decoration: InputDecoration(label: Text(label)),
+        keyboardType: keyboardType,
+        textInputAction: textInputAction,
       );
 }
