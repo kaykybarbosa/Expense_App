b52a099a211ce7486b60966f500ffc081d14dde8
Kayky Barbosa <kaykyb629@gmail.com>
2024-10-14 12:13:09 -0300

    refactor: modificando o buttomSheet para se adaptar aos tipos dos registros

--- lib/pages/home/widgets/custom_show_modal_bottom_sheet.dart
+++ lib/pages/home/widgets/custom_show_modal_bottom_sheet.dart
@@ -1,15 +1,24 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:expense_app/components/custom_text_field.dart';
+import 'package:expense_app/domain/enums/expense_type.dart';
 import 'package:expense_app/utils/constants.dart';
 import 'package:flutter/material.dart';
 
 enum ModalButtomSheetType { create, edit }
+
+extension ModalButtomSheetTypeZ on ModalButtomSheetType {
+  bool get isCreate => this == ModalButtomSheetType.create;
+  bool get isEdit => this == ModalButtomSheetType.edit;
+}
 
 void customShowModalButtomSheet(
   BuildContext context, {
   required void Function()? onPressed,
   required TextEditingController nameController,
   required TextEditingController amountController,
-  ModalButtomSheetType type = ModalButtomSheetType.create,
+  ModalButtomSheetType buttomSheetType = ModalButtomSheetType.create,
+  ExpenseType? expenseType,
 }) async =>
     Future.delayed(
       Duration.zero,
@@ -28,21 +37,33 @@ void customShowModalButtomSheet(
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
+                  /// Title
                   Padding(
                     padding: const EdgeInsets.all(8.0),
-                    child: _Title(type),
+                    child: _Title(
+                      buttomSheetType: buttomSheetType,
+                      expenseType: expenseType ?? ExpenseType.expense,
+                    ),
                   ),
+
+                  /// Name
                   CustomTextField(
                     label: 'Name',
-                    isRequiredFocus: type == ModalButtomSheetType.create,
+                    isRequiredFocus: buttomSheetType.isCreate,
                     controller: nameController,
                   ),
+
+                  /// Amout
                   CustomTextField(
                     label: 'Amount',
                     controller: amountController,
                     keyboardType: const TextInputType.numberWithOptions(decimal: true),
+                    textInputAction: TextInputAction.done,
                   ),
+
                   const SizedBox(height: 30),
+
+                  /// Save
                   SizedBox(
                     width: double.infinity,
                     child: ElevatedButton(
@@ -60,13 +81,23 @@ void customShowModalButtomSheet(
     );
 
 class _Title extends StatelessWidget {
-  const _Title(this.type);
+  const _Title({required this.buttomSheetType, required this.expenseType});
 
-  final ModalButtomSheetType type;
+  final ModalButtomSheetType buttomSheetType;
+  final ExpenseType expenseType;
 
   @override
-  Widget build(BuildContext context) => Text(
-        type == ModalButtomSheetType.create ? 'New Expense' : 'Edit Expense',
-        style: const TextStyle(fontSize: Constants.largeFontSize),
-      );
+  Widget build(BuildContext context) {
+    TextStyle style = const TextStyle(fontSize: Constants.largeFontSize);
+
+    return buttomSheetType.isCreate
+        ? Text(
+            expenseType.isIncome ? 'New Income' : 'New Expense',
+            style: style,
+          )
+        : Text(
+            expenseType.isIncome ? 'Edit Income' : 'Edit Expense',
+            style: style,
+          );
+  }
 }
