fee4f78034a70fcc08e3df52378edb66cd156c7f
Kayky Barbosa <kaykyb629@gmail.com>
2024-10-15 11:38:57 -0300

    refactor: pequena alteração

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,10 @@ import 'package:expense_app/app/expense_app.dart';
 import 'package:expense_app/dependency_injection/app_component.dart';
 import 'package:flutter/material.dart';
 
-import 'database/expense_database.dart';
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  configureDependencies();
-  await ExpenseDatabase.initialize();
+  await configureDependencies();
 
   runApp(const ExpenseApp());
 }
