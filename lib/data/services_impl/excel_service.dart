8c5cd3a13a0e846f6826fb0590b90c71191dd1b5
kaykybarbosa <kaykyb629@gmail.com>
2025-03-06 11:14:08 -0300

    :recycle: refactor: Retafora implementação do ServiceExcel

--- lib/data/services_impl/excel_service.dart
+++ lib/data/services_impl/excel_service.dart
@@ -1,12 +1,14 @@
-import 'dart:developer';
 import 'dart:io';
 
 import 'package:excel/excel.dart';
 import 'package:expense_app/data/extensions/date_time_extension.dart';
+import 'package:expense_app/data/extensions/double_extension.dart';
 import 'package:expense_app/domain/contracts/services/i_excel_service.dart';
 import 'package:expense_app/domain/models/expense.dart';
 import 'package:expense_app/ui/pages/home/home_controller.dart';
 import 'package:expense_app/utils/helper_functions.dart';
+import 'package:expense_app/utils/my_colors.dart';
+import 'package:open_file/open_file.dart';
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
 
@@ -16,50 +18,140 @@ class ExcelService implements IExcelService {
   final HomeController homeController;
 
   @override
-  Future<void> create() async {
+  Future<void> openExpenseReport(File file) async {
+    try {
+      await OpenFile.open(file.path);
+    } catch (_) {}
+  }
+
+  @override
+  Future<File?> createExpenseReport() async {
     // Initialize excel
-    Excel excel = Excel.createExcel();
+    final Excel excel = Excel.createExcel();
+    final CellStyle cellStyle = CellStyle(
+      bold: true,
+      horizontalAlign: HorizontalAlign.Center,
+    );
 
     final List<MonthlySummary> monthlySummary = homeController.monthlySummary2();
 
     for (final monthly in monthlySummary) {
-      final String currentMonth = getMonthByNumber(monthly.month - 1);
-      Sheet sheet = excel['$currentMonth/${monthly.year}'];
-
+      // Expenses
       List<Expense> currentExpenses =
           homeController.expenses
               .where((e) => e.date.month == monthly.month && e.date.year == monthly.year)
               .toList();
 
-      log('${monthly.toString()} - ${currentExpenses.length}', name: 'EXCEL_IMPL_2');
+      if (currentExpenses.isEmpty) continue;
 
-      final int kRowIndex = 3;
+      // Sheet
+      final String currentMonth = getMonthByNumber(monthly.month - 1);
+      final Sheet sheet = excel['$currentMonth/${monthly.year}'];
+
+      // Incomes subtotal
+      final double incomesSubTotal = _calculateExpenses(
+        expenses: currentExpenses,
+        type: ExpenseType.income,
+      );
+      sheet.cell(CellIndex.indexByColumnRow(columnIndex: 0, rowIndex: 1))
+        ..value = TextCellValue('Incomes')
+        ..cellStyle = cellStyle;
+      sheet.cell(CellIndex.indexByColumnRow(columnIndex: 0, rowIndex: 2))
+        ..value = TextCellValue(incomesSubTotal.formatAmount)
+        ..cellStyle = cellStyle.copyWith(
+          fontColorHexVal: ExcelColor.fromHexString(MyColors.successHex),
+        );
+
+      // Expenses subtotal
+      final double expenseSubTotal = _calculateExpenses(
+        expenses: currentExpenses,
+        type: ExpenseType.expense,
+      );
+      sheet.cell(CellIndex.indexByColumnRow(columnIndex: 2, rowIndex: 1))
+        ..value = TextCellValue('Expenses')
+        ..cellStyle = cellStyle;
+      sheet.cell(CellIndex.indexByColumnRow(columnIndex: 2, rowIndex: 2))
+        ..value = TextCellValue(expenseSubTotal.formatAmount)
+        ..cellStyle = cellStyle.copyWith(
+          fontColorHexVal: ExcelColor.fromHexString(MyColors.warnHex),
+        );
+
+      // Sheet header
+      sheet.cell(CellIndex.indexByColumnRow(columnIndex: 0, rowIndex: 4))
+        ..value = TextCellValue('Name')
+        ..cellStyle = cellStyle;
+      sheet.cell(CellIndex.indexByColumnRow(columnIndex: 1, rowIndex: 4))
+        ..value = TextCellValue('Ammount')
+        ..cellStyle = cellStyle;
+      sheet.cell(CellIndex.indexByColumnRow(columnIndex: 2, rowIndex: 4))
+        ..value = TextCellValue('Date')
+        ..cellStyle = cellStyle;
+
+      // Setting values
+      final int initialRowIndex = 6;
       for (var i = 0; i < currentExpenses.length; i++) {
-        int rowIndex = i == 0 ? kRowIndex : kRowIndex + i;
+        int rowIndex = i == 0 ? initialRowIndex : initialRowIndex + i;
         final Expense expense = currentExpenses[i];
 
         List<CellValue> dataList = [
           TextCellValue(expense.name),
-          DoubleCellValue(expense.amount),
-          TextCellValue(expense.date.formatDate),
-
-          TextCellValue(expense.type.label),
+          TextCellValue(expense.amount.formatAmount),
+          TextCellValue(expense.date.formatDate2),
         ];
 
+        // -- Insert values
         sheet.insertRowIterables(dataList, rowIndex);
+
+        // -- Ammount style
+        sheet
+            .cell(CellIndex.indexByColumnRow(columnIndex: 1, rowIndex: rowIndex))
+            .cellStyle = CellStyle(
+          fontColorHex: ExcelColor.fromHexString(
+            (expense.type.isIncome ? MyColors.successHex : MyColors.warnHex),
+          ),
+        );
+
+        // -- Date style
+        sheet
+            .cell(CellIndex.indexByColumnRow(columnIndex: 2, rowIndex: rowIndex))
+            .cellStyle = CellStyle(
+          numberFormat: NumFormat.defaultDate,
+          horizontalAlign: HorizontalAlign.Center,
+        );
       }
     }
 
-    final List<int>? fileBytes = excel.save();
-    final Directory directory = await getApplicationDocumentsDirectory();
-    final String filePath = p.join('${directory.path}/expenses.xlsx');
+    // Deleting sheet that is created automatically.
+    excel.delete('Sheet1');
+
+    return _saveExcel(excel.save());
+  }
+
+  Future<File?> _saveExcel(List<int>? fileBytes) async {
+    final Directory? directory =
+        Platform.isAndroid
+            ? await getDownloadsDirectory()
+            : await getApplicationDocumentsDirectory();
 
     if (fileBytes != null) {
-      File(filePath)
-        ..createSync(recursive: true)
-        ..writeAsBytesSync(fileBytes);
+      final File file =
+          File(p.join('${directory?.path}/expenses.xlsx'))
+            ..createSync(recursive: true)
+            ..writeAsBytesSync(fileBytes);
 
-      log('Arquivo salvo em: $filePath', name: 'EXCEL_IMPL_2');
+      return file;
     }
+
+    return null;
+  }
+
+  double _calculateExpenses({required List<Expense> expenses, ExpenseType? type}) {
+    List<Expense> currentExpenses = expenses;
+
+    if (type != null) {
+      currentExpenses = expenses.where((expense) => expense.type == type).toList();
+    }
+
+    return currentExpenses.fold(0, (sum, expense) => sum + expense.amount);
   }
 }
--- lib/domain/contracts/services/i_excel_service.dart
+++ lib/domain/contracts/services/i_excel_service.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
+
 import 'package:expense_app/dependency_injection/app_component.dart';
 
 abstract class IExcelService {
   static IExcelService get instance => getIt<IExcelService>();
 
-  Future<void> create();
+  Future<void> openExpenseReport(File file);
+
+  Future<File?> createExpenseReport();
 }
